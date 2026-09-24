6591ffd8303725169d6cde8ee13e15e1d896bf85
Auglisters <1263554294@qq.com>
2026-09-24 20:32:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo 'src-git luci-app-airoha-npu https://github.com/rchen14b/luci-app-airoha-npu' >> feeds.conf.default
+echo 'src-git luci-theme-aurora https://github.com/eamonxg/luci-theme-aurora' >> feeds.conf.default
+echo 'src-git luci-app-aurora-config https://github.com/eamonxg/luci-app-aurora-config' >> feeds.conf.default
