bca73a393493fa6630abbbf7c6e797c6be3d6cb7
Auglisters <1263554294@qq.com>
2026-09-25 00:16:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-echo 'src-git luci-app-airoha-npu https://github.com/rchen14b/luci-app-airoha-npu' >> feeds.conf.default
-echo 'src-git luci-theme-aurora https://github.com/eamonxg/luci-theme-aurora' >> feeds.conf.default
-echo 'src-git luci-app-aurora-config https://github.com/eamonxg/luci-app-aurora-config' >> feeds.conf.default
+git clone https://github.com/eamonxg/luci-theme-aurora.git package/luci-theme-aurora
+git clone https://github.com/eamonxg/luci-app-aurora-config.git package/luci-app-aurora-config
