1cb42ea46e1ca09243f887e5def18e5e9f53d778
Auglisters <1263554294@qq.com>
2026-09-26 13:09:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,6 +19,4 @@
 
 git clone https://github.com/eamonxg/luci-theme-aurora.git package/luci-theme-aurora
 git clone https://github.com/eamonxg/luci-app-aurora-config.git package/luci-app-aurora-config
-git clone https://github.com/Loong1996/ImmortalWrt-Airoha package/ImmortalWrt-Airoha
-mv package/ImmortalWrt-Airoha/packages/luci-app-airoha-npu package/
-rm -rf package/ImmortalWrt-Airoha
+git clone https://github.com/luanmuc/luci-app-airoha-npu.git package/luci-app-airoha-npu
